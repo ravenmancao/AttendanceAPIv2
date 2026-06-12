b603f93b3e7af68a98b151038b11d7d3283b537c
ravenmancao <venmancao@gmail.com>
2026-06-12 21:49:50 +0800

    Initial commit

---
+++ Controllers/attendanceController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using attendanceAppService;
+using AttendanceModels;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AttendanceAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AttendanceController : ControllerBase
+    {
+        private readonly AppService _appService;
+
+        private static List<Student> _students = new();
+
+        public AttendanceController()
+        {
+            _appService = new AppService();
+
+            if (!_students.Any())
+            {
+                var s1 = new Student
+                {
+                    Id = "34d7b7df",
+                    Name = "Elle Mar"
+                };
+
+                var s2 = new Student
+                {
+                    Id = "7803ca92",
+                    Name = "Raven"
+                };
+
+                _appService.GenerateAttendance(s1);
+                _appService.ComputeRateAndRemark(s1);
+
+                _appService.GenerateAttendance(s2);
+                _appService.ComputeRateAndRemark(s2);
+
+                _students.Add(s1);
+                _students.Add(s2);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var result = _students.Select(s => new
+            {
+                s.Id,
+                s.Name,
+                s.Rate,
+                s.Remark,
+
+                Attendance = s.Attendance.Select(a => a.ToString()).ToList()
+            });
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(string id)
+        {
+            var student = _students.FirstOrDefault(x => x.Id == id);
+
+            if (student == null)
+                return NotFound(new { message = "Student not found" });
+
+            return Ok(new
+            {
+                student.Id,
+                student.Name,
+                student.Rate,
+                student.Remark,
+                Attendance = student.Attendance.Select(a => a.ToString()).ToList()
+            });
+        }
+    }
+}
---
+++ Models/attendanceViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace AttendanceAPI.Models
+{
+    public class AttendanceViewModel
+    {
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+        public double Rate { get; set; }
+        public string Remark { get; set; } = "";
+
+        // 🔥 ALWAYS STRING OUTPUT (P/A/E)
+        public List<string> Attendance { get; set; } = new();
+    }
+}
